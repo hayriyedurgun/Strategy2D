810e17cf4a358d47a89d2b09f2b27686e6029ce3
hilk <115804595+hayriyedurgun@users.noreply.github.com>
2023-01-06 00:50:46 +0300

    unneccessary lines removed

--- Assets/_Scripts/Managers/GameManager.cs
+++ Assets/_Scripts/Managers/GameManager.cs
@@ -26,35 +26,6 @@ namespace Assets._Scripts
         {
             m_Instance = null;
         }
-
-        //public GameObject Canvas;
-
-        //public Image BuildingImg;
-
-        //public int GridX;
-        //public int GridY;
-
-        //[SerializeField]
-        //private float m_BuildingCurrentSizeX;
-        //[SerializeField]
-        //private float m_BuildingCurrentSizeY;
-
-        //[SerializeField]
-        //private float m_DefaultPosX;
-        //[SerializeField]
-        //private float m_DefaultPosY;
-
-
-        private void Start()
-        {
-            //var buildingRect = BuildingImg.GetComponent<RectTransform>();
-
-            //m_BuildingCurrentSizeX = buildingRect.sizeDelta.x;
-            //m_BuildingCurrentSizeY = buildingRect.sizeDelta.y;
-
-            //m_DefaultPosX = buildingRect.anchoredPosition.x;
-            //m_DefaultPosY = buildingRect.anchoredPosition.y;
-        }
     }
 }
 
